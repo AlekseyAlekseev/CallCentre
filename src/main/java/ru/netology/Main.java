99f2797501182c4bee1ddb5f231bb26fbce58f64
Aleksey Alekseev <a.alekseev5743@yandex.ru>
2021-05-26 18:00:27 +0300

    first commit

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -6,11 +6,16 @@ import java.util.concurrent.PriorityBlockingQueue;
 public class Main {
     public static void main(String[] args) {
 
-
-        final int SLEEP_TIME = 2000;
+        // Пауза между поступлениями звонков
+        final int SLEEP_TIME = 1000;
+        // Количество вызовов за раз
         final int CALLS_PER_SECOND = 60;
+
+        // Генерация случайных телефонных номеров
         Random random = new Random();
         long numbers = random.nextInt(1_000_000_000) + (random.nextInt(90) + 10) * 1_000_000_000L;
+
+
         PriorityBlockingQueue<Long> queue = new PriorityBlockingQueue<>();
 
         Runnable r = () -> {
@@ -18,8 +23,9 @@ public class Main {
                 while (!Thread.currentThread().isInterrupted()) {
                     for (int i = 0; i < CALLS_PER_SECOND; i++) {
                         queue.add(numbers);
-                        System.out.println("От " + Thread.currentThread().getName() + " поступил звонок ");
+
                     }
+                    System.out.println("От " + Thread.currentThread().getName() + " поступило множество звонков");
                     Thread.sleep(SLEEP_TIME);
                 }
             } catch (InterruptedException err) {
