12e95aa1a11bb7b8ec7fd966d959eb4dfdf4b14a
Aleksey Alekseev <a.alekseev5743@yandex.ru>
2021-05-26 17:57:06 +0300

    first commit

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -8,6 +8,7 @@ public class Main {
 
 
         final int SLEEP_TIME = 2000;
+        final int CALLS_PER_SECOND = 60;
         Random random = new Random();
         long numbers = random.nextInt(1_000_000_000) + (random.nextInt(90) + 10) * 1_000_000_000L;
         PriorityBlockingQueue<Long> queue = new PriorityBlockingQueue<>();
@@ -15,9 +16,11 @@ public class Main {
         Runnable r = () -> {
             try {
                 while (!Thread.currentThread().isInterrupted()) {
+                    for (int i = 0; i < CALLS_PER_SECOND; i++) {
+                        queue.add(numbers);
+                        System.out.println("От " + Thread.currentThread().getName() + " поступил звонок ");
+                    }
                     Thread.sleep(SLEEP_TIME);
-                    queue.add(numbers);
-                    System.out.println("От " + Thread.currentThread().getName() + " поступил звонок ");
                 }
             } catch (InterruptedException err) {
                 Thread.currentThread().interrupt();
